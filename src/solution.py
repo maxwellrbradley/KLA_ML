7ef36ba934f279f6996f574fc0540bcd9462e7c9
Max Bradley <maxwell.r.bradley@gmail.com>
2019-03-10 15:30:19 -0700

    About to delete the yield column

--- src/solution.py
+++ src/solution.py
@@ -16,6 +16,11 @@ MULCH_DECREMENT_RATE = 0.15
 def csvReader(file):
     return pd.read_csv(file, sep=",") # open file with , splitter
 
+# MACHINE LEARNING ALGORITHM
+# run data through a particular regression kernel
+# train the model on the data
+# check to see accuracy of model
+# return the regression classifer with the highest score
 
 def algorithmSelector(X_train, y_train, X_test, y_test):
     # take data set, run data sets through each and return the classfier with the best confidence level
@@ -23,109 +28,89 @@ def algorithmSelector(X_train, y_train, X_test, y_test):
     maxConfidence = -1
     indexToReturn = -1
     
-    for num, k in enumerate(['linear','poly','rbf','sigmoid']): # enumerates over all possible types of models
+    for num, k in enumerate(['linear','poly','rbf','sigmoid']): # enumerates over all possible types of kernels
         clf = svm.SVR(kernel=k, gamma='auto')
         clf.fit(X_train, y_train) # train model on X_test and y_test
-        confidence = clf.score(X_test, y_test) # check confidence score
+        confidence = clf.score(X_test, y_test) # check R^2 value, measure of accuracy
         print(f"Confidence: {confidence} with model: {k}") # prints the confidence and model
         classifiers.append(clf) # sets classifier to the back of the array
         if confidence > maxConfidence: # if the confidence level is higher for this model
             maxConfidence = confidence # set max confidence to check which index to return 
-            indexToReturn = num # sets the index to return to be the one in the array
+            indexToReturn = num # sets the index to return the most accurate model
 
     return classifiers[indexToReturn]
+
+
+#SCRIPT BEGINS HERE
             
-
+################################################################################################
+# PROCESS INCOMING DATA
+################################################################################################
+# read in data from csv file
 main_df = csvReader('~/Documents/Work/Internships/KLA_PE/data/farmerData.csv')
-main_df.fillna(0, inplace=True)
-#main_df['Average Watered Per Week(gallons)'] = 0
-
-#main_df = main_df[main_df['Mulch (inches)'] < 8]
-#main_df = main_df[main_df['Fertilizer (cubic feet)'] == 0]
-
-
+# fills parts of incoming data that don't have a value with 0 
+main_df.fillna(0, inplace=True) 
 
 ################################################################################################
-# GET INFORMATION ON LABEL
+# GET INFORMATION ON LABEL (weekly yield of berries)
 ################################################################################################
 
-#print(main_df.info())
-#print(main_df['Weekly Yield (lbs of berries)'].describe())
-
-
-################################################################################################
-# STEP 0: AVERAGE OUT THE AMOUNT OF WATER USED PER WEEK
-################################################################################################
-#totalWatered = 0
-#for index in range(0, len(main_df)):
-#    totalWatered += main_df.at[index,'Watering(gallons)']
-#    if ((index + 1) % 52) == 0: # if on the last part of a row
-#        for i in range(0, 52):
-#            main_df['Average Watered Per Week(gallons)'][index - i] = totalWatered / 52 # average out the amount watered
-#        totalWatered = 0
-
-
-# set up total watered amount of gallons to be the average watered per week to be the rain fall and the average watering
-#main_df['Total Watered(gallons)'] = main_df['Rain(gallons)'] + main_df['Average Watered Per Week(gallons)'] 
+print(main_df.info())
+print(main_df['Weekly Yield (lbs of berries)'].describe())
 
 ################################################################################################
-# PLOT CORRELATION GRAPHS
+# PLOT DISTRIBUTION GRAPH FOR THE OVERALL YIELD
 ################################################################################################
-#plot = sb.distplot(main_df['Weekly Yield (lbs of berries)'],  color='blue', bins=int(100/5))
-#plotter.xlabel('Yield of Berries')
-#plotter.ylabel('Amount of samples?')
-#plotter.show()
-## good for saving distribution of data
-#fig = plot.get_figure()
-#fig.savefig('distribution.png')
+
+# create a plot of the distribution of the output
+plot = sb.distplot(main_df['Weekly Yield (lbs of berries)'],  color='blue', bins=int(100/5))
+plotter.xlabel('Yield of Berries') # labels x axis 
+plotter.ylabel('Samples distributed') # labels y axis
+fig = plot.get_figure() # gets figure
+fig.savefig('distribution.png')# saves figure
 
 ################################################################################################
-# CREATE CORRELATION GRAPH
+# CREATE CORRELATION GRAPH BETWEEN OVERALL YIELD AND ALL OTHER VARIABLES
 ################################################################################################
+
+# deletes mulch and fertizer added because already reflected in fertilizer amount and current mulch
+main_df.drop(inplace=True, columns = ['Mulch added (inches)', 'Fertilizer added (cubic feet)'])
+# get correlations between 8 most correlated columns
+k = 8   
+# create correlation of main dataframe
 correlation = main_df.corr()
-f, ax = plotter.subplots(figsize=(12,9))
-#sb.heatmap(correlation, vmax=0.8, square=True)
-#plotter.show()
-
-#main_df = main_df.drop(columns = ['Watering(gallons)']) # drops unimportant watering in gallons
-#main_df = main_df.drop(columns = ['Average Watered Per Week(gallons)']) # drops watering per week
-#main_df = main_df.drop(columns = ['Rain(gallons)']) # drops the incoming rain
-main_df.drop(inplace=True, columns = ['Mulch added (inches)', 'Fertilizer added (cubic feet)']) # drops the incoming rain
-k = 8   # we'll look at the 10 largest correlations
-correlation = main_df.corr()
+# get columns with 8 largest correlations
 cols = correlation.nlargest(k, 'Weekly Yield (lbs of berries)')['Weekly Yield (lbs of berries)'].index
-#cols = correlation.nlargest(k, 'Moisture %')['Moisture %'].index
-
-
-print('The', k, 'columns with the highest correlation with yield:')
-print(list(cols))   # the k variables that correlate most highly with SalePrice
-
-cm = np.corrcoef(main_df[cols].values.T)
+# get correlations between different variables
+cm = np.corrcoef(main_df[cols].values.T) 
+# gets suplots
 f , ax = plotter.subplots(figsize = (20, 15))
 
 plot = sb.heatmap(cm, 
-            vmax = .9, 
-            linewidths = 0.01, 
+            vmax = .9, # max color shown in heatmap
+            linewidths = 0.01, # linewidth of graph
             square = True, 
             annot = True, 
-            cmap = 'viridis',
+            cmap = 'viridis', # color type
             linecolor = "white", 
-            xticklabels = cols.values, 
-            annot_kws = {'size': 12}, 
-            yticklabels = cols.values)
+            xticklabels = cols.values, # give x tick names of graphs
+            annot_kws = {'size': 12}, # sizing
+            yticklabels = cols.values) # set up ytick names same as x
 
-#plotter.show()
+# save correlation graph
 fig = plot.get_figure()
-fig.savefig('correlation.png')
-print("Saved figure")
-#
+fig.savefig('correlation.png') 
+
 ################################################################################################
 # CREATE SCATTER PLOT
 ################################################################################################
 
+# get columns to create scatter plot between
 columns = ['Weekly Yield (lbs of berries)', 'Moisture %', 'Sun/TempF', 'Fertilizer (cubic feet)', 'Watering (gallons)', 'Nutrition %', 'Mulch (inches)', 'Rain (gallons)']
+# create scatter plot
 sb.set()
-plot = sb.pairplot(main_df[columns],size=2, kind='scatter', vars=columns, diag_kind='kde') #Had to change 'height' to 'size' because of the update in seaborn
+plot = sb.pairplot(main_df[columns],size=2, kind='scatter', vars=columns, diag_kind='kde') 
+# save figure
 plot.savefig('scatter.png')
 
 
@@ -133,29 +118,33 @@ plot.savefig('scatter.png')
 ## STEP 0: CREATE MODEL FOR PREDICTING YIELD OF BERRIES WITH HIGH ACCURACY
  ################################################################################################# 
 
-
-print(f"main_df without cols for overall predictor: {main_df.drop(columns = ['Weekly Yield (lbs of berries)', 'Nutrition %', 'Watering (gallons)', 'Fertilizer (cubic feet)', 'Mulch (inches)', 'Rain (gallons)'])}") 
+# set up the input to the training and testing to be only the moisture % and the sun/tempF 
+#because of their high correlations
 X = np.array(main_df.drop(columns = ['Weekly Yield (lbs of berries)', 'Nutrition %', 'Watering (gallons)', 'Fertilizer (cubic feet)', 'Mulch (inches)', 'Rain (gallons)'])) 
-x = preprocessing.scale(X) # scale the input array to make it easier to use
-y = np.array(main_df['Weekly Yield (lbs of berries)']) # set up output array to be the pounds of berries
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.1) # puts results of testing and training into the X_train, y_train, X_test, and y_test vars. The test size is set up as 0.2, and the 
+# scale the input array to make processing faster
+x = preprocessing.scale(X) 
+# make the output array the thing we want to predict, aka the label
+y = np.array(main_df['Weekly Yield (lbs of berries)']) 
+# set up the arrays to test and train on by splitting up the main data frame into a data input and 
+# output to train on, and then an input and output to test the data on
+# split: 90% train, 10% test 
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.1)
+# set the main overall predictor to be the output of the machine learning algorithm
+# machine learning algorithm defined on line 25
 mainPredictor = algorithmSelector(X_train, y_train, X_test, y_test)
 
 ################################################################################################
-## STEP 1: CREATE RELATIONSHIP TO MOISTURE %
+## STEP 1: CREATE MOISTURE PREDICTION MODEL
 #################################################################################################
-
-# need to keep mulch in there
-# what is the correct relationship?
-print("\n\n\n")
-
-k = 8   # we'll look at the 10 largest correlations
+# look at the 8 largest correlations
+k = 8  
+# drop the label because not looking for correlation between label and moisture
 correlation = main_df.drop(columns=['Weekly Yield (lbs of berries)']).corr()
 cols = correlation.nlargest(k, 'Moisture %')['Moisture %'].index
-
+# set up subplot
 cm = np.corrcoef(main_df[cols].values.T)
 f , ax = plotter.subplots(figsize = (20, 15))
-
+# create correlation plot
 plot = sb.heatmap(cm, 
             vmax = .9, 
             linewidths = 0.01, 
@@ -166,69 +155,41 @@ plot = sb.heatmap(cm,
             xticklabels = cols.values, 
             annot_kws = {'size': 12}, 
             yticklabels = cols.values)
-
-#plotter.show()
+# save the moisture correlation plot
 fig = plot.get_figure()
 fig.savefig('moistureCorrelation.png')
-print("Saved figure")
-
 # next step: 
 # check about relationship to moisture, and see what the biggest correlators are there
-print(f"main_df without cols for moisture: {main_df.drop(columns=['Weekly Yield (lbs of berries)', 'Moisture %', 'Sun/TempF', 'Nutrition %', 'Fertilizer (cubic feet)', 'Rain (gallons)'])}") 
-X = np.array(main_df.drop(columns=['Weekly Yield (lbs of berries)', 'Moisture %', 'Sun/TempF', 'Nutrition %', 'Fertilizer (cubic feet)', 'Rain (gallons)'])) # the best predictor for moisture % I have so far
-X = np.array(main_df[['Mulch (inches)','Watering (gallons)']]) # best moisture model i have so far
-y = np.array(main_df['Moisture %']) # set up output array to be the pounds of berries
-X = preprocessing.scale(X) # scale the input array to make it easier to use
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.1) # puts results of testing and training into the X_train, y_train, X_test, and y_test vars. The test size is set up as 0.2, and the 
+#print(f"main_df without cols for moisture: {main_df.drop(columns=['Weekly Yield (lbs of berries)', 'Moisture %', 'Sun/TempF', 'Nutrition %', 'Fertilizer (cubic feet)', 'Rain (gallons)'])}") 
+# drop all columns except mulch in inches and watering gallons because of their high correlation with moisture %
+X = np.array(main_df[['Mulch (inches)','Watering (gallons)']])
+# set up output array to be the pounds of berries
+y = np.array(main_df['Moisture %']) 
+# scale the input array to make it easier to use
+X = preprocessing.scale(X) 
+# split up testing and training with 90/10 training and testing split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.1)
+# create moisture predictor based on the machine learning algorithm defined on line 25
 moisturePredictor = algorithmSelector(X_train, y_train, X_test, y_test)
-confidence = moisturePredictor.score(X_test, y_test) # get confidence of overall test
-print(f"confidence of water yield: {confidence}")
 
 ################################################################################################
 # STEP 3: READ IN OUTPUT DATASET WITH SUN/TEMPF, RAINFALL, CURRENT MULCH, CURRENT FERT
 # MULCH TO ADD, FERT TO ADD
 ################################################################################################
+# read in the weather data sheet
 output_df = csvReader('~/Documents/Work/Internships/KLA_PE/data/weather.csv')
+# set up the current mulch of the whole system to be 0
 output_df['Current Mulch'] = 0
+# clear the watering of all weeks
 output_df['Watering (gallons)'] = 0
+# clear the yield of all weeks
 output_df['Yield'] = 0 # will this be needed?
 output_df['Mulch added (inches)'] = 0 # will this be needed?
-
+output_df['Fertilizer (cubic feet)'] = 0 # will this be needed?
 output_df['Fertilizer added (cubic feet)'] = 0 # will this be needed?
 ################################################################################################
 # STEP 4: ENTER MAIN ALGORITHM
 ################################################################################################
-
-
-
-
-# mulch, water
-
-#mulch = 0
-#water = 0
-#
-#
-## start mulch at 0, water at 0, increase each until 
-#
-#print("Checking moisture model")
-#i = 0
-#while i < 1000:
-#    mulch += .01
-#    water += 0.1
-#    moistureArray = np.array([mulch, 3]).reshape(1, -1)
-##    print(moistureArray)
-#    predictedMoisture = moisturePredictor.predict(moistureArray)
-#    if predictedMoisture > 0:
-#        
-#        print(f"Array: {moistureArray}")
-#        print(f"Predicted value: {predictedMoisture}")
-#    i += 1
-#
-#while True:
-#    pass
-
-
-
 
 # for each week in the total forcast
 for week in range(0, len(output_df)):
@@ -302,6 +263,8 @@ for week in range(0, len(output_df)):
 
 
 print(f"Final output:\n\n{output_df}")
+
+output_df.to_csv(r'data/output.csv')
 
 
         # add 0.1 inches of mulch to the overall amount
